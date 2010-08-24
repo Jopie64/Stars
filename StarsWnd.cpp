77529349df6a85e70f2be838a4d43db6ba2fd4df
Johan 't Hart <johanthart@gmail.com>
2010-08-24 15:41:40 +0200

    Muis is nu aantrekkelijk

--- StarsWnd.cpp
+++ StarsWnd.cpp
@@ -6,7 +6,24 @@
 #include "StarsWnd.h"
 #include "jstd/Threading.h"
 #include "boost/bind.hpp"
+#include <cmath>
 
+const int G_NbStars = 50000;
+const int G_RandomInitAllVelocity = 2;
+const int G_RandomInitSingleVelocity = 0.1;
+const int G_DownwardGravitation = 0.001;
+const int G_PullerMass = 2;
+
+CPoint CStar::ToScreen(const CPoint& P_pt_Center)
+{
+	return CPoint((int)m_x + P_pt_Center.x, P_pt_Center.y - (int)m_y);
+}
+
+void CStar::ToStar(const CPoint& P_pt_Center, const CPoint& P_pt_Screen)
+{
+	m_x = P_pt_Screen.x - P_pt_Center.x;
+	m_y = P_pt_Center.y - P_pt_Screen.y;
+}
 
 // CStarsWnd
 
@@ -30,6 +47,7 @@ BEGIN_MESSAGE_MAP(CStarsWnd, CWnd)
 	ON_WM_PAINT()
 	ON_WM_CREATE()
 	ON_WM_TIMER()
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 
@@ -41,8 +59,7 @@ int CStarsWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	if (CWnd::OnCreate(lpCreateStruct) == -1)
 		return -1;
 
-
-	m_vStar.resize(100000);
+	m_vStar.resize(G_NbStars);
 
 	RandomInit();
 
@@ -58,14 +75,32 @@ double RandF()
 	return 1.0 * (rand() % 0x8000) / 0x8000;
 }
 
+
+void CStarsWnd::RandomInit(CStar& P_Star, double P_Velocity)
+{
+	P_Star.m_vx = P_Velocity * (RandF() - 0.5);
+	P_Star.m_vy = P_Velocity * (RandF() - 0.5);
+}
+
+
 void CStarsWnd::RandomInit()
 {
-	double velocity = 2;
+	double velocity = G_RandomInitAllVelocity;
 	for(CvStar::iterator i = m_vStar.begin(); i != m_vStar.end(); ++i)
 	{
-		i->m_vx = velocity * (RandF() - 0.5);
-		i->m_vy = velocity * (RandF() - 0.5);
+		RandomInit(*i, velocity);
 	}
+}
+
+void CStarsWnd::SetPullerPos()
+{
+	CPoint W_pt_Mouse;// = GetCurrentMessage()->pt;
+	GetCursorPos(&W_pt_Mouse);
+	ScreenToClient(&W_pt_Mouse);
+
+	CRect W_Rect_Client;
+	GetClientRect(W_Rect_Client);
+	m_Puller.ToStar(W_Rect_Client.CenterPoint(), W_pt_Mouse);
 }
 
 
@@ -73,6 +108,8 @@ void CStarsWnd::OnPaint()
 {
 	CPaintDC dc(this);
 
+
+	SetPullerPos();
 	CRect W_Rect_Client;
 	GetClientRect(W_Rect_Client);
 
@@ -92,15 +129,11 @@ void CStarsWnd::OnPaint()
 	//Hier tekeken
 	W_Dc.FillSolidRect(&W_Rect_Client, RGB(0,0,0));
 
-	CPoint W_pt_Center = W_Rect_Client.CenterPoint();
+	DrawStars(W_Dc);
 
-	for(CvStar::iterator i = m_vStar.begin(); i != m_vStar.end(); ++i)
-	{
-		CPoint W_pt((int)i->m_x + W_pt_Center.x, (int)i->m_y + W_pt_Center.y);
-		if(!W_Rect_Client.PtInRect(W_pt))
-			continue;
-		W_Dc.FillSolidRect(CRect(W_pt, CSize(1,1)), RGB(255,255,255));
-	}
+	CRect W_Rect_Puller(m_Puller.ToScreen(W_Rect_Client.CenterPoint()), CSize(4,4));
+	W_Dc.FillSolidRect(W_Rect_Puller, RGB(255,0,0));
+
 
 
 
@@ -108,9 +141,26 @@ void CStarsWnd::OnPaint()
 	//Hier klaar
 	dc.BitBlt(0,0,W_Rect_Client.Width(), W_Rect_Client.Height(), &W_Dc, 0, 0, SRCCOPY);
 	W_Dc.SelectObject(W_OldBmPtr);
+}
 
+void CStarsWnd::DrawStars(CDC& P_Dc)
+{
+	CRect W_Rect_Client;
+	GetClientRect(W_Rect_Client);
 
-	
+	CPoint W_pt_Center = W_Rect_Client.CenterPoint();
+
+	for(CvStar::iterator i = m_vStar.begin(); i != m_vStar.end(); ++i)
+	{
+		CPoint W_pt(i->ToScreen(W_pt_Center));
+		if(!W_Rect_Client.PtInRect(W_pt))
+		{
+			*i = CStar();
+			RandomInit(*i, G_RandomInitSingleVelocity);
+			continue;
+		}
+		P_Dc.FillSolidRect(CRect(W_pt, CSize(1,1)), RGB(255,255,255));
+	}
 }
 
 void CStarsWnd::Start()
@@ -135,7 +185,20 @@ void CStarsWnd::AsyncRun()
 	{
 		for(CvStar::iterator i = m_vStar.begin(); i != m_vStar.end(); ++i)
 		{
-			i->m_vy += 0.001;
+//			if(i->m_x == 0 && i->m_y == 0)
+//				int i=0;
+			i->m_vy -= G_DownwardGravitation;
+
+			double xoff = i->m_x - m_Puller.m_x;
+			double yoff = i->m_y - m_Puller.m_y;
+			double distSqr = xoff * xoff + yoff * yoff;
+			double dist = sqrt(distSqr);
+			double force = G_PullerMass / distSqr;
+			double factor = force / dist;
+
+
+			i->m_vx -= xoff * factor;
+			i->m_vy -= yoff * factor;
 
 
 			i->m_x += i->m_vx;
@@ -154,4 +217,11 @@ void CStarsWnd::OnTimer(UINT_PTR nIDEvent)
 	{
 	case eT_Invalidate:	Invalidate(FALSE); break;
 	}
+}
+
+void CStarsWnd::OnSize(UINT nType, int cx, int cy)
+{
+	CWnd::OnSize(nType, cx, cy);
+	SetPullerPos();
+	RandomInit();
 }
--- StarsWnd.h
+++ StarsWnd.h
@@ -7,6 +7,10 @@ class CStar
 {
 public:
 	CStar():m_x(0), m_y(0), m_vx(0), m_vy(0){}
+
+	CPoint  ToScreen(const CPoint& P_pt_Center);
+	void	ToStar(const CPoint& P_pt_Center, const CPoint& P_pt_Screen);
+
 	double m_x;
 	double m_y;
 
@@ -34,7 +38,11 @@ protected:
 public:
 	afx_msg void OnPaint();
 
+	void		SetPullerPos();
+	void		DrawStars(CDC& P_Dc);
+
 	void		RandomInit();
+	void		RandomInit(CStar& P_Star, double P_Velocity);
 
 	void		Start();
 	void		Stop();
@@ -50,10 +58,14 @@ private:
 
 
 //Star
-	CvStar m_vStar;
+	CStar	m_Puller;
+	CvStar	m_vStar;
+
+
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 public:
 	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
 
 
